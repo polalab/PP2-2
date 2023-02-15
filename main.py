9d554481851bb629e1615daac6f8d1aec61ab7df
polalab <58389054+polalab@users.noreply.github.com>
2023-02-15 23:38:15 +0100

    OOP

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
 
+# ex 1
+def find_index(element, l):
+    """:parameter: element, list
+        Find on which position in the list is the element
+        If there is no element in the list :return: ‘None’"""
 
+def find_value(element, d):
+    """:parameter: element, dict
+        :return: the value corresponding to the element
+        If the element is not in dict :return: ‘None’"""
+
+
+# ex 2.
+def firt_five(l):
+    """:parameter:: list
+        ::return:: a tuple of first five elements of the list"""
+
+def first_five_to_tuple(l):
+    """:parameter:: list
+    ::return:: a list of first five elements"""
+
+def reverse_list(l):
+    """:parameter:: list
+        ::return:: reverse the list"""
+    
+def convert(s):
+    """::parameter:: string
+        ::return:: turn the string into tuple of letters in the word"""
+
+def convert_on_space(s):
+    """::parameter:: string
+        ::return:: turn the string into tuple of word in the word
+        example:   convert_on_space("Hey, it is Pola here!")
+        returns ("Hey,", "it", "is", "Pola", "here!")
+        """

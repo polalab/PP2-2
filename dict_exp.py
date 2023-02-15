363b18c49db012d3cfc1aff70d7a25b99504e1b7
polalab <58389054+polalab@users.noreply.github.com>
2023-02-15 22:59:19 +0100

    Dictionary

---
+++ dict_exp.py
@@ -0,0 +1,25 @@
+"""Dictionaries"""
+
+# initializing:
+
+student_grades = {}
+
+# adding
+
+student_grades['Katrina'] = 10
+student_grades['Pola'] = 9
+
+# you can mix the types so you have to be careful
+
+
+student_grades['Anna'] = "excellence"
+
+print(student_grades)
+
+# calling
+
+print(student_grades['Katrina'])
+
+# printing the keys and values
+print(student_grades.keys())
+print(student_grades.values())
